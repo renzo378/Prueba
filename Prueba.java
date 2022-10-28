a68d09e257ddbe3c6611df4bed974c3ae22a8650
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-10-27 21:08:12 -0300

    Create Prueba.java

---
+++ Prueba.java
@@ -0,0 +1,9 @@
+public class Prueba{
+  
+ protected static final DFA[] _decisionToDFA;
+	protected static final string DIAS = 7;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		ABSTRACT=1, ASSERT=2, BOOLEAN=3,IDENTIFIER=111; 
+}
