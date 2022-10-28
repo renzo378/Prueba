f51e1176e7f34a8d77f32c523949df4ddfd8d72a
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-10-28 18:09:31 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -5,8 +5,7 @@ public class JavaLexer extends Lexer {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		ABSTRACT=1, ASSERT=2, BOOLEAN=3, BREAK=4, BYTE=5, CASE=6, CATCH=7, CHAR=8, 
-		CLASS=9, CONST=10, CONTINUE=11;
+		ABSTRACT=1, ASSERT=2;
   public static final int
 		HOLA=58;
 }
