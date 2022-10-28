528a6d84fafd42b2b072b13d048073c8efe2ad2c
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-10-27 21:09:33 -0300

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -1,8 +1,7 @@
 public class Prueba{
   
- protected static final DFA[] _decisionToDFA;
+ 
 	protected static final string DIAS = 7;
-	protected static final PredictionContextCache _sharedContextCache =
-		new PredictionContextCache();
+	
 	
 }
