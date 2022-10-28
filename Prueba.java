9f0b3c351cdc0f28e8cd8d73c47e213bc69176d7
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-10-27 21:31:26 -0300

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -1,8 +1,7 @@
 public class Prueba{
 	protected static final String DIAS = 7;
 	public static final String ALGO;
-	protected static final PredictionContextCache _sharedContextCache =
-		new PredictionContextCache();
+	
 	
 	
 }
