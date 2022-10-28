bce483074265c4091d219a794365d450a01275d0
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-10-28 18:06:08 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,10 +1,12 @@
-package pruebaSistema;
+public class JavaLexer extends Lexer {
+	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
 
-@Entity
-public class Persona{
-  private String nombre;
-  private String apellido;
-  public String nombreCompleto(){
-    return this.nombre + this.apellido;
-  }
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		ABSTRACT=1, ASSERT=2, BOOLEAN=3, BREAK=4, BYTE=5, CASE=6, CATCH=7, CHAR=8, 
+		CLASS=9, CONST=10, CONTINUE=11;
+  public static final int
+		HOLA=58;
 }
